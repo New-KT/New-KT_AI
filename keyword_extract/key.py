0811ff4bdfc45c997c7b5a7552fa50457aa5b0e1
taekyung <cerezo3@naver.com>
2023-12-27 16:45:35 +0900

    #key

--- keyword_extract/key.py
+++ keyword_extract/key.py
@@ -2,7 +2,7 @@ from extract import *
 
 def keyword(): 
     load_dotenv()
-    file_path = r"keyword extract\meeting.txt"
+    file_path = r"keyword_extract\meeting.txt"
     file_path= read_concatenate_news(file_path)
     # Call the function and print the result
     result = extract_keywords_from_meeting(file_path)
